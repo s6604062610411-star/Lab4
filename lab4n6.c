f442fca9d8c6499f8c5afc8cf2dbf920047ebcb1
6604062610411Panithan <s6604062610411@email.kmutnb.ac.th>
2026-01-16 20:19:03 +0700

    lab4n6.c

---
+++ lab4n6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j,k,a;   
+    scanf("%d %d",&i,&j);
+    
+    for(k=1;k<=j;k++){
+    	
+    	for(a=1;a<=j;a++)
+    	printf("%d",i);
+    	printf("\n");
+    	
+    	
+	}
+    return 0;
+}
+
+
