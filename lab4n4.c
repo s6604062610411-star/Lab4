dbf3cd37da6053003099ac63a4a25aadff6c1032
6604062610411Panithan <s6604062610411@email.kmutnb.ac.th>
2026-01-16 20:18:17 +0700

    lab4n4.c

---
+++ lab4n4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;   
+
+    i = 1;
+    while (i <= 3) {
+        printf("Row %d: ", i);
+
+        for (j = 1; j <= 5; j++) {
+            printf("%3d", j);
+        }
+
+        printf("\n");
+        i++;
+    }
+
+    return 0;
+}
+
+
